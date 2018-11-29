2cc8dba641cdbda5fb51ec893271a7fe59e140bf
Sophie Kader <skader97@gmail.com>
2018-11-29 11:07:24 -0500

    updates

--- popup.js
+++ popup.js
@@ -6,23 +6,19 @@
 
 let testButton = document.getElementById('testButton');
 
-testButton.onclick = function(element) {
-  chrome.tabs.getSelected(null, 
-      function(tab) { 
-        var tabURL = tab.url;
-        var pingURL = "http://ip-api.com/json/#" + tabURL;
-        var req = new XMLHttpRequest();
-      	req.open("GET", pingURL, true);
-      	req.onreadystatechange = function() {
-          if (req.readyState == 4) {
-            if (req.status == 200) {
-              var obj = JSON.parse(req.responseText); 
-              alert(obj.country);
-              document.write("OK");
-            }
-          }
-        };
-      req.send();
-      });
-};
+chrome.tabs.getSelected(null, function(tab) { 
+  var tabURL = tab.url;
+  var pingURL = "http://ip-api.com/json/#" + tabURL;
+  var req = new XMLHttpRequest();
+  req.open("GET", pingURL, true);
+  req.onreadystatechange = function() {
+    if (req.readyState == 4) {
+      if (req.status == 200) {
+        var obj = JSON.parse(req.responseText); 
+        document.write("<h2>This website IP address is located in " + obj.country + "</h2>");
+      }
+    }
+  };
+  req.send();
+});
 
