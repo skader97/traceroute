917044e977ba83c4e83b80ca4946b7ee6aa18c6a
Sophie Kader <skader97@gmail.com>
2018-11-30 11:28:50 -0500

    ip address works now and onload

--- popup.js
+++ popup.js
@@ -4,19 +4,31 @@
 
 'use strict';
 
-chrome.tabs.getSelected(null, function(tab) { 
-  var tabURL = tab.url;
-  var pingURL = "http://ip-api.com/json/#" + tabURL;
-  var req = new XMLHttpRequest();
-  req.open("GET", pingURL, true);
-  req.onreadystatechange = function() {
-    if (req.readyState == 4) {
-      if (req.status == 200) {
-        var obj = JSON.parse(req.responseText); 
-        document.write("<h2>This website IP address is located in " + obj.country + "</h2>");
-      }
-    }
-  };
-  req.send();
-});
-
+let ipButton = document.getElementById('IPbutton');
+const apiKey = 'a4e2bf0c8ffe58c1a96b98b54d9bfab4'
+// ipButton.onclick = function(element) {
+  chrome.tabs.getSelected(null, 
+      function(tab) { 
+        var tabURL = tab.url;
+        // strip url to just the domain name
+        var match = tabURL.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
+        if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) {
+          tabURL= match[2];
+        } else {
+          tabURL = null;
+        }
+        var pingURL = "http://api.ipapi.com/api/" + tabURL + '?access_key=' + apiKey;
+        // get request to API 
+        var req = new XMLHttpRequest();
+        req.open("GET", pingURL, true);
+        req.onreadystatechange = function() {
+          if (req.readyState == 4) {
+            if (req.status == 200) {
+              var obj = JSON.parse(req.responseText); 
+              document.write("<h2>This website IP address is located in " + obj.country_name + "</h2>");
+            }
+          }
+        };
+      req.send();
+      });
+// };
