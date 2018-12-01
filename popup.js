f423fbc483b2a23570f2c8f4d2c3192dd1fdc3f1
Sophie Kader <skader97@gmail.com>
2018-12-01 08:59:03 -0500

    update

--- popup.js
+++ popup.js
@@ -4,31 +4,38 @@
 
 'use strict';
 
-let ipButton = document.getElementById('IPbutton');
 const apiKey = 'a4e2bf0c8ffe58c1a96b98b54d9bfab4'
-// ipButton.onclick = function(element) {
-  chrome.tabs.getSelected(null, 
-      function(tab) { 
-        var tabURL = tab.url;
-        // strip url to just the domain name
-        var match = tabURL.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
-        if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) {
-          tabURL= match[2];
+alert(imgs.length); 
+chrome.tabs.getSelected(null, 
+function(tab) { 
+  var domain = getHostName(tab.url);
+  
+  var pingURL = "http://api.ipapi.com/api/" + domain + '?access_key=' + apiKey;
+  // get request to API 
+  var req = new XMLHttpRequest();
+  req.open("GET", pingURL, true);
+  req.onreadystatechange = function() {
+    if (req.readyState == 4) {
+      if (req.status == 200) {
+        var obj = JSON.parse(req.responseText); 
+        var country = obj.country_name;
+        var region = obj.region_name; 
+        if (region) {
+          document.write("<h2>This website IP address is located in " + region + ", " + country + "</h2>");
         } else {
-          tabURL = null;
+          document.write("<h2>This website IP address is located in " + country + "</h2>");
         }
-        var pingURL = "http://api.ipapi.com/api/" + tabURL + '?access_key=' + apiKey;
-        // get request to API 
-        var req = new XMLHttpRequest();
-        req.open("GET", pingURL, true);
-        req.onreadystatechange = function() {
-          if (req.readyState == 4) {
-            if (req.status == 200) {
-              var obj = JSON.parse(req.responseText); 
-              document.write("<h2>This website IP address is located in " + obj.country_name + "</h2>");
-            }
-          }
-        };
-      req.send();
-      });
-// };
+      }
+    }
+  };
+  req.send();
+});
+
+function getHostName(url) {
+  var match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
+  if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) {
+    return match[2];
+  } else {
+      return null;
+  }
+}
