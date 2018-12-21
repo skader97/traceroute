06a00f3503caa3e74c1b0047aa9ba4a59f4a5781
Sophie Kader <skader97@gmail.com>
2018-12-20 22:34:53 -0500

    map

---
+++ map.js
@@ -0,0 +1,6 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibGFycy0tIiwiYSI6ImNqNnFydXJ2YTAxZHcyd3MxYWprM2QzcTcifQ.O0NNNRdgVnHzcN3HmXf_NQ';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+});
+console.log('Hello browser');
